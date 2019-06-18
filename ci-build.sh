f6d6b1f05d9701e26c649e964d0a998508aaf68d
zhuharev <unybigk@gmail.com>
2019-06-18 17:43:12 +0300

    add systemctl settings

--- ci-build.sh
+++ ci-build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 GOCACHE=/webmaster/.gocache GOPATH=/webmaster/go /usr/local/go/bin/go build -o whu
+systemctl --user stop whu
+mv whu /home/webmaster/bin/whu
+systemctl --user start whu
